0836c69900bcbd9ea943517059039c5db6fdc258
Shani <sh_hatami84@yahoo.com>
2023-12-17 16:52:12 +0330

    Update Normalization.py

--- Normalization.py
+++ Normalization.py
@@ -4,7 +4,7 @@ def Normalize_Decision_Matrix(Decision_Matrix, Attribute_Type, Normal_Method):
     for i in range(0, len(Attribute_Type)):
         if Attribute_Type[i] == 1:
             Type = "profit"
-        elif Attribute_Type[i] == 0:
+        elif Attribute_Type[i] == 0: 
             Type = "cost"
         else:
             "Please, fill the correct attribute type"
