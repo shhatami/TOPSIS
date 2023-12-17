ced518ddbe998556c7ee1612188878a317114b00
Shani <sh_hatami84@yahoo.com>
2023-12-17 16:51:52 +0330

    Update Normalization.py

--- Normalization.py
+++ Normalization.py
@@ -3,7 +3,7 @@ from Normalization import *
 def Normalize_Decision_Matrix(Decision_Matrix, Attribute_Type, Normal_Method):
     for i in range(0, len(Attribute_Type)):
         if Attribute_Type[i] == 1:
-            Type = "profit"
+            Type = "profit" 
         elif Attribute_Type[i] == 0:
             Type = "cost"
         else:
